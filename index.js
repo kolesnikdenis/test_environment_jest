b977ccb73e9082934e2c075b5c3e1597237e1c69
KolesnikDenis <i@kolesnikdenis.com>
2022-02-16 04:43:55 +0200

    add same change

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("test");
+
+DEPLOYMENT_GROUP_NAME = process.env.username
+
+var AWS = require('aws-sdk'),
+    region = "us-west-2",
+    secretName = `${DEPLOYMENT_GROUP_NAME}`,
+    secret,
+    decodedBinarySecret;
+var fs = require('fs');
+console.log(secretName);
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ${DEPLOYMENT_GROUP_ID}
+echo ${DEPLOYMENT_ID}
+echo deployment-archive
+
